5e241bc81c1f8ddf5a859251f3a31c23d8564acb
github username <github email address>
2020-06-10 15:09:49 -0600

    Initial commit with base React file structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import "../App.css";
-import Nav from "./Nav";
-import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import React, { Component } from "react"
+import "../App.css"
+import Nav from "./Nav"
+import hogs from "../porkers_data"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <HogsPage />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/HogsPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class HogsPage extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+HogsPage.propTypes = {
+
+}
+
+
+export default HogsPage
